e069f438c881da801ba0e4467f6c11227f1163c9
hayashibaracom <comcre.hayashibara@gmail.com>
2022-04-22 17:34:20 +0900

    video_page_create

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,28 +1,49 @@
 class VideosController < ApplicationController
   def index
-    @videos = Video.all
   end
   
   
   def new
     @user = User.find(current_user.id)
     @video = Video.new
-    puts "<------------------------------------------------->"
-    puts current_user.id
   end
 
   def create
     @video = Video.new(video_params.merge(user_id: current_user.id))
-    @video.save
-    redirect_to @video
+    if @video.save
+      flash[:info] = "動画投稿完了！"
+      redirect_to @video
+    else 
+      render new_video_path
+    end
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    if @video.update(video_params.merge(user_id: current_user.id))
+      flash[:success] = "更新完了"
+      redirect_to @video
+    else  
+      render edit_video_path(@video)
+    end
   end
 
   def show
     @user = User.find(current_user.id)
     @video = Video.find(params[:id])
-    puts "<--------------------------------------------------->"
-
   end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    flash[:success] = "Videodeleted"
+    redirect_to @video
+  end
+  
 
   private
 
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,4 @@
 class Video < ApplicationRecord
-  has_one_attached :video
   belongs_to :user
+  has_one_attached :video
 end
